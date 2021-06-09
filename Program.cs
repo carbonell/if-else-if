1b1bc4b5eda0e158b862b9a1cf26f9c533933000
Carlos Carbonell López <carbonelldx89@gmail.com>
2021-06-08 23:12:50 -0400

    First Commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace if_else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que captura una letra mayuscula y la convierte en minuscula");
+            Console.WriteLine("Coloque una letra: ");
+            var letter = Console.ReadLine();
+            if (letter == "A")
+            {
+                Console.WriteLine("La minuscula correspondiente es: a");
+            }
+            else if (letter == "B")
+            {
+                Console.WriteLine("La minuscula correspondiente es: b");
+            }
+            else
+            {
+                Console.WriteLine("El valor que digitaste no es valido");
+            }
+
+        }
+    }
+}
